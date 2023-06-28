f44d0e5ebf516f8aa09463c9b0e81e839d833c37
Jordan Russell <hellointeractivejo@gmail.com>
2023-06-28 11:41:15 -0500

    set up prisma

--- momentum-coffee-app/src/app/layout.js
+++ momentum-coffee-app/src/app/layout.js
@@ -1,11 +1,9 @@
-import Nav from './components/layout/Nav'
 import './globals.css'
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Nav />
       <body >{children}</body>
     </html>
   )
