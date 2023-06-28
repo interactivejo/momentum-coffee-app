fa5787f7a36782ea30d6bed88797c547a8ab7bb1
Jordan Russell <hellointeractivejo@gmail.com>
2023-06-28 17:32:24 -0500

    updated mobile hours styling

--- coffee-app/app/components/single/HoursMobile.tsx
+++ coffee-app/app/components/single/HoursMobile.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Hours = () => {
   return (
     <div
-        className="text-xs text-white"
-    >
-        <p>M, W, F 9am-4pm</p>
-        <p>Tu, Th 9am-3pm</p>
-    </div>
+    className="block lg:hidden text-xs font-Obviously font-light"
+>
+    <p><span className='font-medium'>M, W, F </span>9am-4pm</p>
+    <p><span className='font-medium'>Tu, Th </span>9am-3pm</p>
+</div>
   )
 }
 
