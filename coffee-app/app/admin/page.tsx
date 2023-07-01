4f61e557cd0481744b8180b76f57f5262b4ea9ca
Jordan Russell <hellointeractivejo@gmail.com>
2023-07-01 18:59:18 -0500

    removed deleted componenet

--- coffee-app/app/admin/page.tsx
+++ coffee-app/app/admin/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Container from '../components/layout/Container'
-import TabsMenu from '../components/ui/TabsMenu'
+
 
 export default function AdminPage() {
   return (
     <Container>
-      
-        <TabsMenu />
-
-        
     
     </Container>
   )
