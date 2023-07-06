f790a91eb61a974ba3e759c70967be70f4a62a34
Jordan Russell <hellointeractivejo@gmail.com>
2023-07-06 14:36:00 -0500

    added link to logo

--- coffee-app/app/components/ui/Logo.tsx
+++ coffee-app/app/components/ui/Logo.tsx
@@ -2,19 +2,23 @@
 import React from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Logo = () => {
     const router = useRouter()
 
 
     return (
-        <Image
+        <Link href='/'>
+            <Image
             src='/images/logo.svg'
             alt='Momentum Coffee Logo'
             className='cursor-pointer'
             height="100"
             width="100"
         />
+        </Link>
+        
     )
 }
 
---
+++ coffee-app/app/order/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../components/layout/Container'
+
+const OrderPage = () => {
+  return (
+    <div>
+        <Container>
+          <h1>Order Page</h1>
+        </Container>
+    </div>
+  )
+}
+
+export default OrderPage
