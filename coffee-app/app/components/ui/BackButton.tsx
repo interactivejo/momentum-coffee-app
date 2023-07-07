aefd391d35d72fd5d06cbdaa192642696ec2c605
Jordan Russell <hellointeractivejo@gmail.com>
2023-07-07 14:52:34 -0500

    fixed back button & began order review section

--- coffee-app/app/components/ui/BackButton.tsx
+++ coffee-app/app/components/ui/BackButton.tsx
@@ -1,15 +1,29 @@
+import Link from "next/link";
 import React from "react";
 import { BsArrowLeft } from "react-icons/bs";
 
 const BackButton = (props: any) => {
   return (
-    <div className="flex flex-row items-center gap-2 cursor-pointer"  onClick={() => props.setIsDrawerOpen(false)}>
-      <BsArrowLeft
-        size={20}
-        className="text-lg text-[#F15D2A]"
-       
-      />
-      <p className="text-[#F15D2A] font-Futura font-semibold">Back</p>
+    <div>
+      {props.href ? (
+        <div>
+          <Link
+            href={props.href}
+            className="flex flex-row items-center gap-2 cursor-pointer"
+          >
+            <BsArrowLeft size={20} className="text-lg text-[#F15D2A]" />
+            <p className="text-[#F15D2A] font-Futura font-semibold">Back</p>
+          </Link>
+        </div>
+      ) : (
+        <div
+          className="flex flex-row items-center gap-2 cursor-pointer"
+          onClick={() => props.setIsDrawerOpen(false)}
+        >
+          <BsArrowLeft size={20} className="text-lg text-[#F15D2A]" />
+          <p className="text-[#F15D2A] font-Futura font-semibold">Back</p>
+        </div>
+      )}
     </div>
   );
 };
--- coffee-app/app/order/page.tsx
+++ coffee-app/app/order/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Container from '../components/layout/Container'
+import BackButton from '../components/ui/BackButton'
+import OrderReview from '../components/sections/OrderReview'
 
 const OrderPage = () => {
   return (
     <div>
         <Container>
-          <h1>Order Page</h1>
+          <div className='mb-8'>
+            <BackButton href='/'/>
+          </div>
+
+          <OrderReview />
         </Container>
     </div>
   )
