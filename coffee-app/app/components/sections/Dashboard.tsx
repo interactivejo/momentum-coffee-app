8c4d3b22d69d2844e37d231fafbce0668488a979
Jordan Russell <hellointeractivejo@gmail.com>
2023-07-12 11:51:22 -0500

    built out more of the dashboard section

--- coffee-app/app/components/sections/Dashboard.tsx
+++ coffee-app/app/components/sections/Dashboard.tsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import TabsMenu from '../layout/TabsMenu'
-import MenuHeader from '../layout/MenuHeader'
-import DashboardStats from '../ui/DashboardStats'
-
-
-
+import React from "react";
+import TabsMenu from "../layout/TabsMenu";
+import MenuHeader from "../layout/MenuHeader";
+import DashboardStats from "../ui/DashboardStats";
+import OrderHistoryTable from "../ui/OrderHistoryTable";
 
 const Dashboard = () => {
   return (
     <div>
-        <MenuHeader 
-          title="Momentum Cafe Dashboard"
-          downloadButton={true}
-        />
+      <MenuHeader title="Momentum Cafe Dashboard" downloadButton={true} />
 
-        <div>
-          <DashboardStats />
+      <div className="mb-8">
+        <DashboardStats />
+      </div>
+
+      <div className="flex flex-col w-full gap-8 lg:flex-row">
+        <div className="lg:w-[50%]">
+          <h3 className="mb-4 text-2xl font-semibold uppercase font-ObviouslyNarrow">
+            Drinks Per Month
+          </h3>
+
+          <div className="border-[1px] min-h-[20rem]"></div>
+        </div>
+
+        <div className="lg:w-[50%] mb-8">
+          <h3 className="mb-4 text-2xl font-semibold uppercase font-ObviouslyNarrow">
+            Drinks By Category
+          </h3>
+          <div className="border-[1px] min-h-[20rem]"></div>
+        </div>
+      </div>
+
+      <div>
+      <h3 className="mb-4 text-2xl font-semibold uppercase font-ObviouslyNarrow">
+            Recent Orders
+          </h3>
+          <OrderHistoryTable />
         </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
